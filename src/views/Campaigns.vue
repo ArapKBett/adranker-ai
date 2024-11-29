92fd63dff82767f072c61f84f72c6b9bfcebf67f
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:49:05 -0500

    Create Campaigns.vue

---
+++ src/views/Campaigns.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Campaigns</h1>
+    <CampaignList @select="selectCampaign" />
+  </div>
+</template>
+
+<script>
+import CampaignList from '@/components/CampaignList.vue';
+
+export default {
+  name: 'Campaigns',
+  components: {
+    CampaignList,
+  },
+  methods: {
+    selectCampaign(campaign) {
+      this.$router.push({ name: 'CampaignDetail', params: { id: campaign.id } });
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
