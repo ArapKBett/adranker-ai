1165c90cde4d2dd1d0c49c23b0e0bec462ef72c6
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:44:51 -0500

    Create CampaignDetails.vue

---
+++ src/components/CampaignDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="campaign">
+    <h2>{{ campaign.name }}</h2>
+    <p>Details about the campaign...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    campaign: Object,
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #333;
+}
+</style>
