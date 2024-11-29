68a616cc3c877bf4502853ce79adb566e2b68869
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:48:13 -0500

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to AdRanker AI</h1>
+    <p>Optimize your Facebook Ad campaigns with ease.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
