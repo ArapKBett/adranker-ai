f70bac4da1dbb4b0aced0cddb9eee749091b0f88
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:52:03 -0500

    Create CreateAd.vue

---
+++ src/views/CreateAd.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Create New Ad</h1>
+    <AdCreator @create="createAd" />
+  </div>
+</template>
+
+<script>
+import AdCreator from '@/components/AdCreator.vue';
+
+export default {
+  name: 'CreateAd',
+  components: {
+    AdCreator,
+  },
+  methods: {
+    createAd(newAd) {
+      console.log('New Ad Created:', newAd);
+      // Handle ad creation logic here
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
