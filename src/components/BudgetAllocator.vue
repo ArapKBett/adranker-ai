07c06a95d632d81847ad86e6bba855c60637c289
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:45:50 -0500

    Create BudgetAllocator.vue

---
+++ src/components/BudgetAllocator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Budget Allocator</h2>
+    <input type="number" v-model="budget" />
+    <button @click="allocateBudget">Allocate Budget</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      budget: 0,
+    };
+  },
+  methods: {
+    allocateBudget() {
+      this.$emit('allocate', this.budget);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+input {
+  margin-right: 10px;
+}
+</style>
