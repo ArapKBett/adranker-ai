8d2bf6ad8f4ff9d9329eea2bb8b69024117155d9
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:49:49 -0500

    Create CampaignDetail.vue

---
+++ src/views/CampaignDetail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Campaign Details</h1>
+    <CampaignDetails :campaign="campaign" />
+  </div>
+</template>
+
+<script>
+import CampaignDetails from '@/components/CampaignDetails.vue';
+
+export default {
+  name: 'CampaignDetail',
+  components: {
+    CampaignDetails,
+  },
+  data() {
+    return {
+      campaign: null,
+    };
+  },
+  created() {
+    const campaignId = this.$route.params.id;
+    // Fetch campaign details based on campaignId
+    this.campaign = { id: campaignId, name: `Campaign ${campaignId}` }; // Example data
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
