63a9bd81345cf1c468d938a09b21a1c32442ee2d
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-29 17:43:39 -0500

    Create CampaignItem.vue

---
+++ src/components/CampaignItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li @click="selectCampaign">
+    {{ campaign.name }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    campaign: Object,
+  },
+  methods: {
+    selectCampaign() {
+      this.$emit('select', this.campaign);
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  cursor: pointer;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+li:hover {
+  background-color: #f0f0f0;
+}
+</style>
